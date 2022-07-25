20e14ba615312926ac6027a01c8105eceacd116a
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-07-25 23:41:34 +0300

    trottle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+try {
+  player.on(
+    'timeupdate',
+    throttle(function (data) {
+      localStorage.setItem(
+        'videoplayer-current-time',
+        JSON.stringify(data.seconds)
+      );
+    }, 5000)
+  );
 
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+} catch (error) {
+  console.log(error);
+}
