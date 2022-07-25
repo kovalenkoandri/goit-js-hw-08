4dc4159b4d0276ed5b5ea3621f3361d26473f93d
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-07-25 12:47:35 +0300

    01 gallery adopted

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,15 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
+const divGallery = document.querySelector('div[class="gallery"]');
+divGallery.innerHTML = galleryItems
+  .map(
+    e => `<a class="gallery__item" href="${e.original}">
+  <img class="gallery__image" src="${e.preview}" alt="${e.description}" /></a>`
+  )
+  .join('');
+var lightbox = new SimpleLightbox('.gallery .gallery__item', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
