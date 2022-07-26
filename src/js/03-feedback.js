f48fd9bbd802c3b02a02e885c8e958b8fb3de5db
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-07-26 17:16:19 +0300

    Cannot read properties of null reading 'elements'

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,27 @@
+var throttle = require('lodash.throttle');
 
 const input = document.querySelector('input');
 const textArea = document.querySelector('textarea');
 const form = document.querySelector('.feedback-form');
 
-input.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
-textArea.value = JSON.parse(localStorage.getItem('feedback-form-state')).message;
-
-form.addEventListener('input', event => {
-  const obj = {
-    email: event.currentTarget.elements.email.value,
-    message: event.currentTarget.elements.message.value,
-  };
-  localStorage.setItem('feedback-form-state', JSON.stringify(obj));
+const savedSettings = localStorage.getItem('feedback-form-state');
+const parsedSettings = JSON.parse(savedSettings);
+input.value = parsedSettings.email;
+textArea.value = parsedSettings.message;
+form.addEventListener(
+  'input',
+  throttle(event => {
+    const obj = {
+      email: event.currentTarget.elements.email.value,
+      message: event.currentTarget.elements.message.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(obj));
+  }, 500)
+);
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(parsedSettings, parsedSettings.email, parsedSettings.message); // settings object
+  localStorage.setItem('feedback-form-state', '{"email":"", "message":""}');
+  input.value = '';
+  textArea.value = '';
 });
